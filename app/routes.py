98fe2770a3e05bdb7bd6dd1cfaf3e335bfa62261
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2022-12-12 23:24:38 -0300

    added sql alchemy and migrations

--- app/routes.py
+++ app/routes.py
@@ -6,15 +6,15 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Luis'}
     posts = [
         {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'author': {'username': 'Michael'},
+            'body': 'Husky'
         },
         {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
+            'author': {'username': 'Jocelin'},
+            'body': 'Poodle'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
